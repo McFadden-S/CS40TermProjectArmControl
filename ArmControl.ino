179a0178b6545832852e200e9d8b1c3a9d63f803
McFadden-S <shaefromhome@shaw.ca>
2019-03-31 17:15:48 -0500

    v1.1
    bug fixes and minor tweaks

--- ArmControl.ino
+++ ArmControl.ino
@@ -1,19 +1,19 @@
 #include <Muscle.h>
 
-const int frequency = 50; // frequency of calls for data in miliseconds
+const int frequency = 100; // frequency of calls for data in miliseconds
 const int valLen = 10;
 
-int PinkieP = 23;
-int RingP = 22;
-int MiddleP = 25;
-int PointerP = 24;
-int ThumbP = 26;
+int PinkieP = 27;
+int RingP = 26;
+int MiddleP = 29;
+int PointerP = 28;
+int ThumbP = 25;
 
-int PinkieA = 30;
-int RingA = 31;
-int MiddleA = 32;
-int PointerA = 33;
-int ThumbA = 35;
+int PinkieA = 34;
+int RingA = 35;
+int MiddleA = 36;
+int PointerA = 37;
+int ThumbA = 32;
 
 int ProMusclePins[] = {PinkieP, RingP, MiddleP, PointerP, ThumbP};
 Muscle ProMuscles[valLen/2];
@@ -26,7 +26,7 @@ void setup() {
   delay(200);
   
   pinSetUp();
-  fullValveTest();
+  //fullValveTest();
 
   for(int i = 0; i < valLen/2; i++){
     ProMuscles[i] = Muscle(ProMusclePins[i]);
@@ -35,8 +35,8 @@ void setup() {
 
   clearBuffer();
   
-  while(Serial.available() == 0){}//end wait for java input
-  clearBuffer();
+  //while(Serial.available() == 0){}//end wait for java input
+  //clearBuffer();
 }//end of set up
 
 void loop() {
@@ -45,20 +45,29 @@ Serial.write('#'); //requests data
 while(Serial.available() == 0){}//end wait for java input
 delay(50);
 
+
 for (int i = 0; i<valLen/2; i++){
       int input = Serial.read();
       if(input == '1'){
-        ProMuscles[i].Hold();
         AntiMuscles[i].Release();
+        ProMuscles[i].Hold();
       }//end of if contracting
-      else{
+      else if(input == '0'){
         ProMuscles[i].Release();
         AntiMuscles[i].Hold();
-      }//end of else opening
+      }//end of else if opening
+      else{
+        for(int j = 0; i < 5; i++){
+          ProMuscles[i].Release();
+          AntiMuscles[i].Release();
+        }//end of for every finger
+      }//end of else ending
       Serial.read();
 }//end of for each finger
 
 delay(frequency); //waits set amount of time
+
+
 }//end of main loop
 
 void pinSetUp(){
@@ -79,7 +88,7 @@ void fullValveTest(){
 
 void blinkValve(int pin){
   digitalWrite(pin, LOW);
-    delay(100);
+    delay(1000);
   digitalWrite(pin, HIGH);
 }//end of blinkValve
 
